b5623a8b88ff16492bc734ed69b859dc95b237a6
shubham <shrivastavashubham596@gmail.com>
2024-05-16 03:35:48 +0530

    Initial Deploy Code

--- src/pages/admin/management/newproduct.tsx
+++ src/pages/admin/management/newproduct.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import {  FormEvent, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import AdminSidebar from "../../../components/admin/AdminSidebar";
